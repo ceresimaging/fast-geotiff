4d6303354749eddf562d4a1d4c925398c4cacb27
Seth Nickell <snickell@gmail.com>
2019-08-01 09:52:21 -1000

    Release working-again version

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/env'],
+            presets: ['@babel/preset-env'],
           }
         }
       }
